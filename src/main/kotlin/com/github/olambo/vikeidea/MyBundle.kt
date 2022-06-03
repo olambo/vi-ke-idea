230b95909822fa8887addd0637d2fb4eeb549511
GitHub Action <action@github.com>
2022-06-03 22:22:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/olambo/vikeidea/MyBundle.kt
+++ src/main/kotlin/com/github/olambo/vikeidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.olambo.vikeidea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/olambo/vikeidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/olambo/vikeidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.olambo.vikeidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.olambo.vikeidea.MyBundle
 
 class MyApplicationService {
 
